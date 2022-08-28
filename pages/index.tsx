e3e6fc96c14b8f6804c80aac23fadc63d105ff79
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-27 20:52:09 -0600

    Fix FAQ page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <Link href='/faq'>
+        <a>FAQ</a>
+      </Link>
+    </>
   )
 }
 
